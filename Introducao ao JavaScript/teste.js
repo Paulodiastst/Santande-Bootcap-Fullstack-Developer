6e0ae575b25ee7e071c406424f666f2c4d9fcd1b
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-06-02 20:03:15 -0300

    Introdução ao JavaScript

---
+++ Introducao ao JavaScript/teste.js
@@ -0,0 +1,14 @@
+/*Manipulando um arquivo*/
+
+/* var preco = 2;
+var desconto = 0.2;
+const PRECO = 0.2;
+var total = PRECO - desconto;*/
+
+/* Funções */
+
+function soma(a, b) {
+  return a + b; // 8
+}
+
+soma(3, 5);
